ae600b531b1f9b224a286fa9fc093761723f66af
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-06-10 07:22:41 +1200

    select option binding solved

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -8,35 +8,38 @@ const Add = () => {
     const [gender, setGender] = useState('');
     const [quantity, setQuantity] = useState('');
     const [desc, setDesc] = useState('');
+    const [img, setImg] = useState();
     // const [product, setProduct] = useState({});
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        
         console.log(title, price, gender, quantity, desc);
+        e.preventDefault();
     };
 
     return (
         <div className="add">
             <form className="form" onSubmit={handleSubmit}>
                 <label className="a11y-hidden">Title</label>
-                <input type="text" className="title" placeholder="title" value={title} onChange={(e) => {setTitle(e.target.value)}} required />
+                <input type="text" className="title" placeholder="title" value={title} onChange={(e) => { setTitle(e.target.value) }} required />
                 <label className="a11y-hidden">Price</label>
-                <input type="text" className="title" placeholder="price in NZD" value={price} onChange={(e) => {setPrice(e.target.value)}} required />
+                <input type="number" className="title" placeholder="price in NZD" value={price} onChange={(e) => { setPrice(e.target.value) }} required />
                 <label className="a11y-hidden">Gender</label>
-                <select name="gender" id="gender" value={gender} onChange={(e) => {setGender(e.target.value)}} required>
+                <select className="gender" defaultValue="" onChange={(e) => { setGender(e.target.value) }} required>
+                    <option value="" disabled>Select Gender..</option>
                     <option value="women">women</option>
                     <option value="men">men</option>
                     <option value="unisex">unisex</option>
                 </select>
                 <label className="a11y-hidden">Quantity</label>
-                <input type="number" className="quantity" placeholder="quantity" value={quantity} onChange={(e) => {setQuantity(e.target.value)}} required />
+                <input type="number" className="quantity" placeholder="quantity" value={quantity} onChange={(e) => { setQuantity(e.target.value) }} required />
                 <label className="a11y-hidden">Description</label>
-                <textarea className="description" value={desc} onChange={(e) => {setDesc(e.target.value)}} required></textarea>
+                <textarea className="description" value={desc} onChange={(e) => { setDesc(e.target.value) }} required></textarea>
+                <label className="a11y-hidden">Image</label>
+                <input type="file" className="img" value={img} onChange={(e) => { setImg(e.target.value) }} required />
                 <Button>Add</Button>
             </form>
         </div>
     );
 }
- 
+
 export default Add;
