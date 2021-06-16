98b503848a12474f1881e88ff81320115e3c30ca
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-06-17 07:01:53 +1200

    commented out code removed

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,15 +3,6 @@ import Button from "./Button";
 
 const CartItem = ({ cartItem, deleteFromCart }) => {
     return (
-        // <li className="cart-item">
-        //     <img className="img" src={cartItem.img} alt="#" />
-        //     <div className="cont">
-        //         <h3 className="title">{cartItem.title}</h3>
-        //         <p className="price-quantity">${cartItem.price} x {cartItem.userQ}</p>
-        //         <Button btnStyle="btn-outline--red" onClick={() => { deleteFromCart(cartItem) }}>Delete</Button>
-        //     </div>
-        // </li>
-
         <li className="cart-item">
             <div className="img-cont">
                 <img className="img" src={cartItem.img} alt="" />
